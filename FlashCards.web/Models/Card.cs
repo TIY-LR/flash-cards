b2b4eb16ed436540b8faa4a15d9c99c8d0083d7a
Brandon Goza <brgoza@gmail.com>
2015-08-21 23:29:44 -0500

    added name property to card model

--- FlashCards.web/Models/Card.cs
+++ FlashCards.web/Models/Card.cs
@@ -12,6 +12,7 @@ namespace FlashCards.web.Models
     public class Card
     {
         public Guid Id { get; set; }
+        public string Name { get; set; }
         public string Description { get; set; }
 
 
