8106affe7a9ce2dec45d505ced8f82deeb3b4173
Brandon Goza <brgoza@gmail.com>
2015-08-22 17:21:55 -0500

    testing seed data.

--- FlashCards.web/Migrations/Configuration.cs
+++ FlashCards.web/Migrations/Configuration.cs
@@ -13,6 +13,7 @@ namespace FlashCards.web.Migrations
         public Configuration()
         {
             AutomaticMigrationsEnabled = true;
+            AutomaticMigrationDataLossAllowed = true;
         }
 
         protected override void Seed(FlashCards.web.Models.ApplicationDbContext context)
