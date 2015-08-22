8e2d841e707f2480320ca5872cfa3c305a3917d0
Brandon Goza <brgoza@gmail.com>
2015-08-22 11:19:00 -0500

    Added other controllers

---
+++ FlashCards.web/Controllers/coursesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using FlashCards.web.Models;
+
+namespace FlashCards.web.Controllers
+{
+    public class coursesController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/courses
+        public IQueryable<course> Getcourses()
+        {
+            return db.courses;
+        }
+
+        // GET: api/courses/5
+        [ResponseType(typeof(course))]
+        public IHttpActionResult Getcourse(Guid id)
+        {
+            course course = db.courses.Find(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(course);
+        }
+
+        // PUT: api/courses/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult Putcourse(Guid id, course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != course.id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(course).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!courseExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/courses
+        [ResponseType(typeof(course))]
+        public IHttpActionResult Postcourse(course course)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.courses.Add(course);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (courseExists(course.id))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = course.id }, course);
+        }
+
+        // DELETE: api/courses/5
+        [ResponseType(typeof(course))]
+        public IHttpActionResult Deletecourse(Guid id)
+        {
+            course course = db.courses.Find(id);
+            if (course == null)
+            {
+                return NotFound();
+            }
+
+            db.courses.Remove(course);
+            db.SaveChanges();
+
+            return Ok(course);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool courseExists(Guid id)
+        {
+            return db.courses.Count(e => e.id == id) > 0;
+        }
+    }
+}
